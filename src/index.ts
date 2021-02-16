ab206830249a4ea5b78ab2b135c85c603cfc40c7
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-02-16 20:21:01 +0900

    testing rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
 
-of(1,2,3).pipe(map(x => x + '!!!')).subscribe(console.log);
-
+of(1, 2, 3)
+  .pipe(map((x) => x + "!!!"))
+  .subscribe(console.log);
