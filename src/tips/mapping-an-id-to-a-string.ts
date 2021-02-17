f9240a922486c18d367e527ebede6772d0ebc672
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-02-18 00:42:14 +0900

    Mapping an id to a string

---
+++ src/tips/mapping-an-id-to-a-string.ts
@@ -0,0 +1,41 @@
+import { combineLatest, of } from "rxjs";
+import { map } from "rxjs/operators";
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  category?: string;
+}
+
+interface ProductCategory {
+  id: string;
+  name: string;
+  description?: string;
+}
+
+const products: Product[] = [
+  { id: "A-1", name: "A", price: 100 },
+  { id: "B-1", name: "B", price: 200 },
+  { id: "C-1", name: "C", price: 300 },
+];
+
+const productCategoies: ProductCategory[] = [
+  { id: "A-1", name: "BIG" },
+  { id: "B-1", name: "SMALL" },
+  { id: "C-1", name: "NORMAL" },
+];
+
+const productsWithCategory$ = combineLatest([
+  of(products), // プライマリデータストリーム
+  of(productCategoies), // ルックアップストリーム
+]).pipe(
+  map(([products, categoies]) =>
+    products.map((product) => ({
+      ...product,
+      category: categoies.find((c) => product.id === c.id)?.name, // 文字列をルックアップ
+    }))
+  )
+);
+
+productsWithCategory$.subscribe((ret) => console.log(ret));
