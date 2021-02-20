5b11ffec2e2f119e4a253f6c289368382a7ced5b
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-02-20 22:42:49 +0900

    startWith 補足

--- src/operators/startWith.ts
+++ src/operators/startWith.ts
@@ -1,5 +1,8 @@
 import { of } from "rxjs";
 import { startWith } from "rxjs/operators";
 
+// 初期値を提供する演算子
+// Initial Valueが欲しい場合はBehaviorSubjectも候補になる
+
 // デフォルト値が必要な場合などで使用する
 of(1, 2, 3).pipe(startWith(10)).subscribe(console.log); // =>c 10 1 2 3
